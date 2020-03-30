8e96e5d5541d37c96e16a15e06c882f2ac553b8d
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-03-29 21:16:41 -0700

    Finished the eventListener

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+
+window.addEventListener('load', () => {
+
+    const rate = .20;
+    let amount;
+    let tipResult;
+
+    let $ = (id) => {
+        return window.document.getElementById(id);
+    }
+
+    $('btn').addEventListener("click", function() {
+        tipResult = $('amount').value * rate
+        $('results').innerHTML = "You should tip $"+tipResult.toFixed(2)+" on $"+$('amount').value;
+    })
+    console.log($('amount').value)
+
+});
